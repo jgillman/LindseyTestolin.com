70761d291b800a70447fa01ec587150fd094e924
Joel G <joeltgillman@gmail.com>
2010-03-30 18:58:27 -0400

    merged stacey and old site

---
+++ public/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<?php include("head.php"); ?>
+
+<div class="vid_row">
+<div class="video_holder">
+&times; JOHNNY BUNKO TRAILER
+<br />
+2008 - promotion for NYT bestselling author dan pink
+<br />
+<object width="500" height="375">	<param name="allowfullscreen" value="true" />	<param name="allowscriptaccess" value="always" />	<param name="movie" value="http://www.vimeo.com/moogaloop.swf?clip_id=864322&amp;server=www.vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933&amp;fullscreen=1" />	<embed src="http://www.vimeo.com/moogaloop.swf?clip_id=864322&amp;server=www.vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="500" height="375"></embed></object>
+</div>
+
+<div class="video_holder2">
+&times; AN AVERAGE IDEA
+<br />
+2007 - music video for the band "love in october"
+<br />
+<object type="application/x-shockwave-flash" width="500" height="375" data="http://www.vimeo.com/moogaloop.swf?clip_id=399946&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933">	<param name="quality" value="best" />	<param name="allowfullscreen" value="true" />	<param name="scale" value="showAll" />	<param name="movie" value="http://www.vimeo.com/moogaloop.swf?clip_id=399946&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933" /></object>
+</div>
+
+</div>
+
+<div class="vid_row">
+<div class="video_holder">
+&times; MOTION REEL
+<br />
+2007 - enjoy my motion reel
+<br />
+<object width="500" height="375">	<param name="allowfullscreen" value="true" />	<param name="allowscriptaccess" value="always" />	<param name="movie" value="http://www.vimeo.com/moogaloop.swf?clip_id=261359&amp;server=www.vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933&amp;fullscreen=1" />	<embed src="http://www.vimeo.com/moogaloop.swf?clip_id=261359&amp;server=www.vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="500" height="375"></embed></object>
+</div>
+
+<div class="video_holder2">
+&times; BUSINESS &Eacute;LECTROCARD
+<br />
+2008 - fun, effective, low-budget electrocard. transmitted by iphone.
+<br />
+<object width="500" height="358">	<param name="allowfullscreen" value="true" />	<param name="allowscriptaccess" value="always" />	<param name="movie" value="http://www.vimeo.com/moogaloop.swf?clip_id=1161029&amp;server=www.vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933&amp;fullscreen=1" />	<embed src="http://www.vimeo.com/moogaloop.swf?clip_id=1161029&amp;server=www.vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="500" height="358"></embed></object>
+</div>
+</div>
+
+<div class="vid_row">
+<div class="video_holder">
+&times; ELECTRICITY AND RUINS
+<br />
+2007 - pushing 216 inflated condiment bags across minneapolis
+<br />
+<object type="application/x-shockwave-flash" width="500" height="375" data="http://www.vimeo.com/moogaloop.swf?clip_id=438829&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933">	<param name="quality" value="best" />	<param name="allowfullscreen" value="true" />	<param name="scale" value="showAll" />	<param name="movie" value="http://www.vimeo.com/moogaloop.swf?clip_id=438829&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933" /></object>
+</div>
+
+<div class="video_holder2">
+&times; OLIVE
+<br />
+2006 - waiting for the olive to chill 
+<br />
+<object type="application/x-shockwave-flash" width="500" height="375" data="http://www.vimeo.com/moogaloop.swf?clip_id=432703&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933">	<param name="quality" value="best" />	<param name="allowfullscreen" value="true" />	<param name="scale" value="showAll" />	<param name="movie" value="http://www.vimeo.com/moogaloop.swf?clip_id=432703&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933" /></object><br />
+</div>
+</div>
+
+<div class="vid_row">
+<div class="video_holder">
+&times; ANIMATED FORMS
+<br />
+2006 - parts iii and iv
+<br />
+<object type="application/x-shockwave-flash" width="500" height="375" data="http://www.vimeo.com/moogaloop.swf?clip_id=432700&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933">	<param name="quality" value="best" />	<param name="allowfullscreen" value="true" />	<param name="scale" value="showAll" />	<param name="movie" value="http://www.vimeo.com/moogaloop.swf?clip_id=432700&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933" /></object>
+</div>
+
+<div class="video_holder2">
+&times; SPICK, SPAN, DISTRIBUTE
+<br />
+2005 - ye animated film of olde 16mm
+<br />
+<object type="application/x-shockwave-flash" width="500" height="375" data="http://www.vimeo.com/moogaloop.swf?clip_id=432653&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933">	<param name="quality" value="best" />	<param name="allowfullscreen" value="true" />	<param name="scale" value="showAll" />	<param name="movie" value="http://www.vimeo.com/moogaloop.swf?clip_id=432653&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933" /></object>
+</div>
+</div>
+
+<div class="vid_row">
+<div class="video_holder">
+&times; ROBOTS
+<br />
+2004 - perhaps the best use of robots and radiohead
+<br />
+<object type="application/x-shockwave-flash" width="500" height="375" data="http://www.vimeo.com/moogaloop.swf?clip_id=434563&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933">	<param name="quality" value="best" />	<param name="allowfullscreen" value="true" />	<param name="scale" value="showAll" />	<param name="movie" value="http://www.vimeo.com/moogaloop.swf?clip_id=434563&amp;server=www.vimeo.com&amp;fullscreen=1&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=ff9933" /></object>
+</div>
+</div>
+
+
+<!-- END MAIN CONTENT -->
+</div>
+
+<div id="footer">
+</div>
+
+<!--
+<div class="video_holder">
+&times; TITLE - 2008
+<br />
+descript
+<br />
+
+</div>
+-->
+
+<?php include("foot.php"); ?>
